15242510acc15aab6619fd4b1e5e39dcf5a0eb53
Пользователь <@DESKTOP-E7N1RLV>
2023-12-26 19:17:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) запрашивает с клавиатуры два целых числа, и выводит на экран сумму данных чисел:
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a}+{b}={a + b}");
+
+
+            //2)  запрашивает с клавиатуры два целых числа,
+            //и выводит на экран сумму данных чисел в прямом и обратном порядке:
+
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a}+{b}={b}+{a}");
+
+            //3) запрашивает с клавиатуры два вещественных числа,
+            //и выводит на экран произведение данных чисел
+            //(вещественные числа выводятся с точностью до 1 знака после запятой):
+
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a:f1}*{b:f1}={a*b:f1}");
+
+            //4)запрашивает с клавиатуры три вещественных числа, и выводит на экран следующее сообщение
+            //(вещественные числа выводятся с точностью до 2 знаков после запятой)
+
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"({a:f2}+{b:f2}) + {c:f2} = {a:f2}+({b:f2}+{c:f2})");
+
+            //5) запрашивает с клавиатуры два вещественных числа,
+            //и выводит на экран результат деления первого числа на второе
+            //(вещественные числа выводятся с точностью до 3 знаков после запятой):
+
+            //Console.Write("a=");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.Write("d=");
+            //double d = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a:f2}/{b:f2}+{c:f2}/{d:f2}= {((a/b)+(c/d)):f2}");
+
+
+            //6)запрашивает с клавиатуры два целых числа,
+            //и выводит на экран результат их суммы, разности и произведения:
+
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write($"a+b={a}+{b}={a + b}\ta-b={a}-{b}={a - b}\ta*b={a}*{b}={a * b}");
+
+            //7)запрашивает с клавиатуры четыре целых числа,
+            //и выводит на экран результат умножения первого числа на третье минус произведение второго числа на четвертое:
+
+            //Console.Write("a =");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b =");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("c =");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.Write("d =");
+            //int d = int.Parse(Console.ReadLine());
+            //Console.Write($"a*c-b*d={a}*{c}-{b}*{d}={(a*c)-(b*d)}");
+
+            //8) запрашивает с клавиатуры три целых числа и выводит следующее сообщение [a*(b+c)]=[(b+c)*a]:
+
+            //Console.Write("a =");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b =");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("c =");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.Write($"[a*(b+c)]=[(b+c)*a]:\t [{a}*({b}+{c})]=[({b}+{c})*{a}]={(b + c) * a}");
+        }
+    }
+}
